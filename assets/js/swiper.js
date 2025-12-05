1345c2483f928a103d662b548930d9adbf371712
aliyev98 <maharram.aliyev98@gmail.com>
2025-12-05 16:55:55 +0400

    project developed

---
+++ assets/js/swiper.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".my-swiper", {
+    loop: true,
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+        delay: 1000,
+        disableOnInteraction: false,
+    },
+    speed: 500,
+});
