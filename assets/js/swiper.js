4d5cb137b0a3d233d8cde71878fc0054f191f04f
aliyev98 <maharram.aliyev98@gmail.com>
2025-12-11 16:50:41 +0400

    portfolio details added

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,10 +1,35 @@
+// const swiper = new Swiper(".my-swiper", {
+//     loop: true,
+//     slidesPerView: 1,
+//     spaceBetween: 0,
+//     autoplay: {
+//         delay: 1000,
+//         disableOnInteraction: false,
+//     },
+//     speed: 500,
+// });
+
+
 const swiper = new Swiper(".my-swiper", {
     loop: true,
-    slidesPerView: 1,
-    spaceBetween: 0,
+    speed: 600,
     autoplay: {
-        delay: 1000,
+        delay: 2000,
         disableOnInteraction: false,
     },
-    speed: 500,
+});
+
+// Səhifə açılan kimi autoplay-i dayandırırıq
+swiper.autoplay.stop();
+
+const swiperEl = document.querySelectorAll(".my-swiper");
+
+// Hover olanda autoplay başlasın
+swiperEl.addEventListener("mouseenter", () => {
+    swiper.autoplay.start();
+});
+
+// Mouse çəkiləndə yenə dayansın
+swiperEl.addEventListener("mouseleave", () => {
+    swiper.autoplay.stop();
 });
