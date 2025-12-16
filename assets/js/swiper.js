a2f16b6ab2b08d41d80b21a046398ca94815cdc4
aliyev98 <maharram.aliyev98@gmail.com>
2025-12-16 16:15:01 +0400

    some pages fixed and responsive design improved

--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,43 +1,6 @@
-// const swiper = new Swiper(".my-swiper", {
-//     loop: true,
-//     slidesPerView: 1,
-//     spaceBetween: 0,
-//     autoplay: {
-//         delay: 1000,
-//         disableOnInteraction: false,
-//     },
-//     speed: 500,
-// });
-
-
-// const swiper = new Swiper(".my-swiper", {
-//     loop: true,
-//     speed: 600,
-//     autoplay: {
-//         delay: 2000,
-//         disableOnInteraction: false,
-//     },
-// });
-
-// // Səhifə açılan kimi autoplay-i dayandırırıq
-// swiper.autoplay.stop();
-
-// const swiperEl = document.querySelectorAll(".my-swiper");
-
-// // Hover olanda autoplay başlasın
-// swiperEl.addEventListener("mouseenter", () => {
-//     swiper.autoplay.start();
-// });
-
-// // Mouse çəkiləndə yenə dayansın
-// swiperEl.addEventListener("mouseleave", () => {
-//     swiper.autoplay.stop();
-// });
-
-
 const swiperEls = document.querySelectorAll(".my-swiper");
 
-swiperEls.forEach((el) => {
+    swiperEls.forEach((el) => {
     const swiperInstance = new Swiper(el, {
         loop: true,
         speed: 600,
@@ -47,7 +10,6 @@ swiperEls.forEach((el) => {
         },
     });
 
-    // İlk açılışda autoplay dayansın
     swiperInstance.autoplay.stop();
 
     el.addEventListener("mouseenter", () => {
@@ -58,3 +20,4 @@ swiperEls.forEach((el) => {
         swiperInstance.autoplay.stop();
     });
 });
+ 
