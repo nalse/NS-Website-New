42550609f598b48bdf4514532da0c47514c2ac3a
aliyev98 <maharram.aliyev98@gmail.com>
2026-01-06 18:26:15 +0400

    change language added, about us changed, portfolio details badgets changed

--- assets/js/accordion.js
+++ assets/js/accordion.js
@@ -6,22 +6,9 @@ document.addEventListener('DOMContentLoaded', function () {
             const expanded = header.getAttribute('aria-expanded') === 'true';
             const panel = header.nextElementSibling;
 
-            // Hazırda çoxlu panel eyni anda açıq qala bilər
             header.setAttribute('aria-expanded', String(!expanded));
             panel.hidden = expanded;
 
-            // Tək panel açıq qalsın istəyirsənsə, aşağıdakı bloku aç:
-            /*
-            if (!expanded) {
-              headers.forEach((h) => {
-                if (h !== header) {
-                  h.setAttribute('aria-expanded', 'false');
-                  const p = h.nextElementSibling;
-                  if (p) p.hidden = true;
-                }
-              });
-            }
-            */
         });
     });
 });
